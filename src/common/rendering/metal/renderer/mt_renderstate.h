43ec724ad1e44aaec40b2bec39190e3639b66f79
John Curley <johndanielcurley@gmail.com>
2026-01-15 17:11:32 +1300

    Metal Renderer: Fix MtRenderState destructor compilation error
    - Removed redundant = default from header to match the custom destructor implementation in the .cpp file.

--- src/common/rendering/metal/renderer/mt_renderstate.h
+++ src/common/rendering/metal/renderer/mt_renderstate.h
@@ -23,7 +23,7 @@ class MtTextureImage;
 class MtRenderState final : public FRenderState {
 public:
   MtRenderState(MetalRenderDevice *fb);
-  ~MtRenderState() = default;
+  ~MtRenderState();
 
   // Draw commands
   void ClearScreen() override;
